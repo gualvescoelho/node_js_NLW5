30e729abd5739b7bc1718793352eb6428105be9d
gustavo <gualvescoelho@gmail.com>
2021-04-19 10:14:14 -0300

    Node.js
    Dia 1 Node.js NLW#5 Rocketseat

---
+++ src/server.ts
@@ -0,0 +1,44 @@
+import express from "express";
+
+//os 3 pontinhos significa que as dependencias dessa lib  "express" e precisa instalar as tipagens
+//da mesma, vindo separadas, e para baixar usar:
+//yarn add @types/express -D (-D significa dependencia de desenvolvimento)
+//yarn add typescript -D, adicionar o typescript no arquivo
+//yarn tsc --init, criar um arquivo de configuração do typescript
+
+const app = express();
+
+/*
+    GET = Buscas
+    POST = Criação
+    PUT = Alteração
+    DELETE = Deletar
+    PATCH = Alterar uma informação específica
+*/
+
+app.get("/", (request, response ) => {
+    return response.send("Olá NLW 05");
+}) //request vem da requisição e o response é a resposta pro cliente
+
+//outra maneira abaixo
+
+/*
+app.get("/", (request, response ) => {
+    return response.json({
+        message: "Olá NLW 05"
+    });
+}) //request vem da requisição e o response é a resposta pro cliente
+*/
+
+
+app.post("/users", (request, response) => {
+    return response.json({ message: "Usuário salvo com sucesso!" })
+})
+
+
+//3333 é uma porta de conexão
+app.listen(3333, () => console.log("Server ir running on port 3333"));
+
+//deu erro devido não poder deixar o import e o from no codigo
+//para ele entender o import e tals, o tradutor é:
+//yarn add ts-nose-dev -D
